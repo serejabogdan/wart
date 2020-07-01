7352f2ed6b61270ec7b67e82ba5d783c69520677
Hasst <sergey1112345@gmail.com>
2020-07-01 17:44:30 +0300

    added eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 80,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    tabWidth: 4,
+    semi: true
+};
